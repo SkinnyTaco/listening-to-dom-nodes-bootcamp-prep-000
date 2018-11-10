5bc50392a0100d930eb69110bc3b3a83bc9c2986
Daniel Dawson <dannyldawson@gmail.com>
2018-11-10 20:59:20 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,10 @@
 // using this file is optional
 // you can also load the code snippets in using your browser's console
+let $ = document.querySelector();
+
+let main = document.querySelector('main');
+main.addEventListener('click', (e) => {
+  main.style.fontWeight = 'bold';
+  main.style.fontSize = '50';
+  alert('I was click!');
+});
