0783525689067e7e4e07044009ee3dd6ad531dad
Daniel Dawson <dannyldawson@gmail.com>
2018-11-10 20:41:55 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -1,2 +1,8 @@
 // using this file is optional
 // you can also load the code snippets in using your browser's console
+let $ = document.querySelector();
+
+let title = $('title');
+title.addEventListener('click', (e) => {
+  
+});
