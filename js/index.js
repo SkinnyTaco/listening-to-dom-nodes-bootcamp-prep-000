2f6f8e41e9d7dca94fd8d2a1923468a874fc1939
Daniel Dawson <dannyldawson@gmail.com>
2018-11-10 20:45:59 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 // you can also load the code snippets in using your browser's console
 let $ = document.querySelector();
 
-let main = $('main');
+let main = document.querySelector('main');
 main.addEventListener('click', (e) => {
   main.style.fontWeight = 'bold';
   main.style.fontSize = '50';
